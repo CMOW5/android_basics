9b1a66b0b98d46d6f772f2fe3bca2e29953f6d60
CMOW5 <tec.cmos@gmail.com>
2017-12-28 01:45:58 -0500

    XML drawables

---
+++ nerd-ranch/beatbox/BeatBox4/app/src/main/java/com/cristian/beatbox1/Sound.java
@@ -0,0 +1,44 @@
+package com.cristian.beatbox1;
+
+/**
+ * Created by cristian on 21/12/17.
+ */
+
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+
+    /* In the constructor, you do a little work to make a presentable
+    name for your sound. First, you split off the filename using
+    String.split(String) . Once you have done that, you use
+    String.replace(String, String) to strip off the file extension, too.
+    */
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+}
